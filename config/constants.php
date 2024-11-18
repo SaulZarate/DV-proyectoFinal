1a2ea9701d8a73bd93a81f56133e0fc9b3c51dd1
Saul Zarate <saul.zarate@davinci.edu.ar>
2024-11-18 15:53:53 -0300

    CREDENTIAL SMTP agregadas

--- config/constants.php
+++ config/constants.php
@@ -1,9 +1,21 @@
 <?
-
-
 date_default_timezone_set("America/Argentina/Buenos_Aires");
 
+/* ----------------------------- */
+/*              PROYECT          */
+/* ----------------------------- */
 define("PATH_SERVER", $_SERVER["DOCUMENT_ROOT"] . "/proyectoFinal/");
 define("DOMAIN_NAME", "http://localhost/proyectoFinal/");
 define("DOMAIN_ADMIN", "http://localhost/proyectoFinal/admin/");
 define("APP_NAME", "TurApp");
+
+
+/* --------------------- */
+/*          SMTP         */
+/* --------------------- */
+define("SMTP_SERVER", "smtp.gmail.com");
+define("SMTP_SECURITY", "SSL");
+define("SMTP_PORT", 465);
+define("SMTP_USERNAME", "saul.zarate@davinci.edu.ar");
+define("SMTP_PASSWORD", "idxzqszrovhpctlk");
+define("SMTP_FROM_EMAIL", "saul.zarate@davinci.edu.ar");
