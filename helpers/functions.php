391b3207c235ff3764171e00541be9cd5a0f57f5
Saul Zarate <saul.zarate@davinci.edu.ar>
2024-11-29 13:08:09 -0300

    upload producción

--- helpers/functions.php
+++ helpers/functions.php
@@ -5,6 +5,7 @@ if (!function_exists('getallheaders')) {
         $headers = [];
         
         if(isset($_SERVER["CONTENT_TYPE"])) $headers["Content-Type"] = $_SERVER["CONTENT_TYPE"];
+        if(!isset($_SERVER["Authorization"]) && isset($_SERVER["PHP_AUTH_USER"]) && isset($_SERVER["PHP_AUTH_PW"])) $headers["Authorization"] = "Basic ".base64_encode($_SERVER["PHP_AUTH_USER"].":".$_SERVER["PHP_AUTH_PW"]);
 
         foreach ($_SERVER as $name => $value) {
             if (substr($name, 0, 5) == 'HTTP_') {
